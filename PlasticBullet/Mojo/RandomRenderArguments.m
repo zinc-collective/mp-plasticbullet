611a4e307af293ff28baf0488c4820b4b46435a1
Sean Clark Hess <seanhess@gmail.com>
2016-07-20 13:30:01 -0600

    border scale: 1.3 to 1.6, 4:3 noborder ratio

--- PlasticBullet/Mojo/RandomRenderArguments.m
+++ PlasticBullet/Mojo/RandomRenderArguments.m
@@ -105,11 +105,11 @@ float randomPercent(float min, float max) {
     BorderArguments border;
 	border.x = (arc4random_uniform(2)) * 1.0f; // 0.00 or 1.00
 	border.y = (arc4random_uniform(2)) * 1.0f; // 0.00 or 1.00
-    border.scale = randomPercent(1.2, 1.4); // (arc4random_uniform(301)) / 1000.0f + 1.0f;
+    border.scale = randomPercent(1.3, 1.6); // (arc4random_uniform(301)) / 1000.0f + 1.0f;
 	border.doScale = (arc4random_uniform(2)) * 1.0f; // 0.00 or 1.00
 	
     int numBorderTypes = 3;
-    int noBorderWeight = 2;
+    int noBorderWeight = 4;
     
     int borderTypeValue = arc4random_uniform(numBorderTypes + noBorderWeight);
     
