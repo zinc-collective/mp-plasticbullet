e701ff08a76e73ce393fc0026ec4db316686a59c
theInternauts <cricketw@wearetheinternauts.com>
2020-01-14 17:57:10 +0900

    FIRST PASS adding UI and visual elements for landing page

---
+++ PlasticBullet/PlasticBullet/BTN_Camera.swift
@@ -0,0 +1,21 @@
+//
+//  BTN_Camera.swift
+//  PlasticBullet
+//
+//  Created by Christopher Wallace on 1/13/20.
+//  Copyright © 2020 Zinc Technology Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct BTN_Camera: View {
+    var body: some View {
+        Image("splash-camera")
+    }
+}
+
+struct BTN_Camera_Previews: PreviewProvider {
+    static var previews: some View {
+        BTN_Camera()
+    }
+}
---
+++ PlasticBullet/PlasticBullet/BTN_Library.swift
@@ -0,0 +1,21 @@
+//
+//  BTN_Library.swift
+//  PlasticBullet
+//
+//  Created by Christopher Wallace on 1/13/20.
+//  Copyright © 2020 Zinc Technology Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct BTN_Library: View {
+    var body: some View {
+        Image("splash-library")
+    }
+}
+
+struct BTN_Library_Previews: PreviewProvider {
+    static var previews: some View {
+        BTN_Library()
+    }
+}
