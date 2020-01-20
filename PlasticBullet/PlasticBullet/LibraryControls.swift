7b2e00beb6097e4de8b9581d38536bf81ae4c257
theInternauts <cricketw@wearetheinternauts.com>
2020-01-20 23:16:06 +0900

    add Image library selection view

--- PlasticBullet/PlasticBullet/LibraryControls.swift
+++ PlasticBullet/PlasticBullet/LibraryControls.swift
@@ -8,14 +8,38 @@
 
 import SwiftUI
 
-struct LibraryControls: View {
-    var body: some View {
-        Text("Library")
+struct LibraryControls: UIViewControllerRepresentable {
+    @Binding var isShowingLibraryControls:Bool
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<LibraryControls>) -> UIViewController {
+        let controller = UIImagePickerController()
+        controller.delegate = context.coordinator
+        return controller
+    }
+    func makeCoordinator() -> LibraryControls.Coordinator {
+        return Coordinator(parent: self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: LibraryControls
+        init(parent: LibraryControls) {
+            self.parent = parent
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let selectedImage = info[.originalImage] as? UIImage {
+                print([selectedImage])
+            }
+            self.parent.isShowingLibraryControls = false
+        }
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<LibraryControls>) {
+        
     }
 }
 
 struct LibraryControls_Previews: PreviewProvider {
     static var previews: some View {
-        LibraryControls()
+        LibraryControls(isShowingLibraryControls: .constant(false))
     }
 }
