73f9181bc8b93ba729dacc497a0b25d5cbb656ce
theInternauts <cricketw@wearetheinternauts.com>
2020-01-28 23:48:26 +0900

    Updated Panel_Info to read text from markdown files

--- PlasticBullet/PlasticBullet/Panel_Info.swift
+++ PlasticBullet/PlasticBullet/Panel_Info.swift
@@ -11,36 +11,69 @@ import SwiftyMarkdown
 
 struct Panel_Info: View {
     @Binding var useFullResolution:Bool
+    @Binding var isShowingInfoView:Bool
+    
+    @State private var first: SwiftyMarkdown?
+    @State private var second: SwiftyMarkdown?
+    @State private var p1: String = ""
+    @State private var p2: String = ""
+    @State private var p3: String = ""
     
     var body: some View {
         VStack {
             HStack {
-//                Button(){}
+                Button(action: {
+                    self.isShowingInfoView.toggle()
+                }) {
+                    Image(systemName: "arrow.left")
+                }
+                Spacer()
                 Image("logo")
                 Spacer()
             }
-            Text("Plastic Bullet offers an infinitie variety of distressed, vintage treatments for your photos. You'll never see the exact same processing twice, and your results are uniquely yours!")
-            Spacer()
-            Text("Love Plastic Bullet? We'd be thrilled if you'd write a review!")
+                .padding()
+                .foregroundColor(Color.white)
+                .background(Color.gray)
+            
+            Text(p1)
+                .padding()
+            
             HStack {
                 Toggle(isOn: $useFullResolution) {
                     Text("Unlimited Resolution")
                 }
             }
+                .padding()
+                .foregroundColor(Color.white)
+                .background(Color.gray)
+                        
+            Text(p2)
+                .padding()
+                .foregroundColor(Color.white)
             Spacer()
-            Text("Turn this on and Plastic Bullet will process the full resolution of your photos, which takes a little longer.")
-            Spacer()
-        }.padding()
+        }
+        .foregroundColor(Color.white)
+        .accentColor(Color.white)
+        .background(Color.black)
+        .onAppear(perform: {
+            self.first = self.loadMD(name: "info-text1")
+            self.second = self.loadMD(name: "info-text2")
+            self.p1 = (self.first?.attributedString().string)!
+            self.p2 = (self.second?.attributedString().string)!
+//            self.p1 = "Plastic Bullet offers an infinite variety of distressed, vintage treatments for your photos. You'll never see the exact same processing twice, and your results are uniquely yours!"
+//            self.p2 = "Love Plastic Bullet? We'd be thrilled if you'd write a review!\nQuestions, comments, or suggestions? Contact us."
+//            self.p3 = "Turn this on and Plastic Bullet will process the full resolution of your photos, which takes a little longer."
+            
+//            print((self.first?.attributedString().string)!)
+        })
     }
     
-//    func loadMD(name:String) -> SwiftyMarkdown? {
-//        if let url = Bundle.mainBundle.URLForResource(name, withExtension: "md") {
-//            return SwiftyMarkdown(url: url)
-//        }
-//        return nil
-//    }
+    func viewDidLoad() {
+//        super.viewDidLoad()
+        print("view did load")
+    }
     
-    func loadMD(name:String) -> SwiftyMarkdown? {
+    func loadMD(name:String) -> SwiftyMarkdown {
         if let url = Bundle.main.url(forResource: name, withExtension: "md"), let md = SwiftyMarkdown(url: url) {
             md.h2.fontName = "AvenirNextCondensed-Bold"
             md.h2.color = UIColor.red
@@ -57,6 +90,6 @@ struct Panel_Info: View {
 
 struct Panel_Info_Previews: PreviewProvider {
     static var previews: some View {
-        Panel_Info(useFullResolution: .constant(false))
+        Panel_Info(useFullResolution: .constant(true), isShowingInfoView: .constant(true))
     }
 }
