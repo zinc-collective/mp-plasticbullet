e1e8bed1da9edec6b84ebb5f351cc2f13bd954c3
theInternauts <cricketw@wearetheinternauts.com>
2021-04-11 06:58:19 +0900

    adding toast message for user feedback whant saving via UIActivityView

--- PlasticBullet/PlasticBullet/Views/ActivityView.swift
+++ PlasticBullet/PlasticBullet/Views/ActivityView.swift
@@ -13,13 +13,25 @@ struct ActivityView: UIViewControllerRepresentable {
     
     var activityItems: [Any] = []
     var applicationActivities: [UIActivity]? = nil
+    let excludedActivityTypes: [UIActivity.ActivityType]? = nil
+    var callback: Callback? = nil
 
+    typealias Callback = (_ activityType: UIActivity.ActivityType?, _ completed: Bool, _ returnedItems: [Any]?, _ error: Error?) -> Void
+    
     func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityView>) -> UIActivityViewController {
-        let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+        let controller = UIActivityViewController(
+            activityItems: activityItems,
+            applicationActivities: applicationActivities)
+        controller.excludedActivityTypes = excludedActivityTypes
+        controller.completionWithItemsHandler = callback        
         return controller
     }
 
     func updateUIViewController(_ uiViewController: UIActivityViewController, context: UIViewControllerRepresentableContext<ActivityView>) {}
+    
+    func testCallback(_ activityType: UIActivity.ActivityType?, _ completed: Bool, _ returnedItems: [Any]?, _ error: Error?){
+        print("test callback")
+    }
 
 }
 
