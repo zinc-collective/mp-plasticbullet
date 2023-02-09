a7c0876415a3e46739aee4dde9b4261e397b4dfb
theinternauts <cricketw@WeAreTheInternauts.com>
2023-02-09 22:43:36 +0900

    Simplify Filter abstraction (a little) and incorporate a "CIColorClamp" filter into the render chain as a test

--- PlasticBullet/ModuleLens.swift
+++ PlasticBullet/ModuleLens.swift
@@ -7,31 +7,35 @@
 //
 
 import SwiftUI
+import CoreImage
 
-struct Filter {
-    let filterName: String
-    var filterEffectValue: Any?
-    var filterEffectValueName: String?
-    
-    init(filterName: String, filterEffectValue: Any?, filterEffectValueName: String?) {
-        self.filterName = filterName
-        self.filterEffectValue = filterEffectValue
-        self.filterEffectValueName = filterEffectValueName
-    }
-}
+@objc class Filter: CIFilter {}
 
 class ModuleLens: ObservableObject {
+    let context = CIContext()
+    
     //  CORE IMAGE FILTERS REFERENCE: "https://developer.apple.com/library/archive/documentation/GraphicsImaging/Reference/CoreImageFilterReference/index.html#//apple_ref/doc/uid/TP40004346-Reference"
-    var sepia: Filter = Filter(filterName: "CISepiaTone", filterEffectValue: 0.95, filterEffectValueName: kCIInputIntensityKey)
-    var gaussianBlur: Filter = Filter(filterName: "CIGaussianBlur", filterEffectValue: 20, filterEffectValueName: kCIInputRadiusKey)
-        
+    //  CORE IMAGE FILTERS REFERENCE: "https://developer.apple.com/documentation/coreimage/processing_an_image_using_built-in_filters"
+    //  NOTE: key notation -->  kCI + {paramName} + Key   i.e.: "InputIntensity" ---> kCIInputIntensityKey
+    
+    var sepia: Filter = Filter(name: "CISepiaTone", parameters: ["inputIntensity": 0.95])!
+    var gaussianBlur: Filter = Filter(name: "CIGaussianBlur", parameters: ["inputRadius": 20])!
+    var photoEffectFade: Filter = Filter(name: "CIPhotoEffectFade")!
+    var photoEffectTransfer: Filter = Filter(name: "CIPhotoEffectTransfer")!
+    
+    
+    // NEED TO SET VALUES
+    var colorClamp: Filter = Filter(name: "CIColorClamp", parameters: ["inputMinComponents":00, "inputMaxComponents":00])!
+    
+    
+    
     init(){
         
     }
     
     func updateFliterableImageView(source: UIImage) -> UIImage {
         print("tapped on a FliterableImageView")
-        self.gaussianBlur.filterEffectValue = 50
+        self.gaussianBlur.setValue(50, forKey: "inputRadius")
         return self.processImage(image: source, filterEffect: self.gaussianBlur)
     }
     
@@ -47,11 +51,14 @@ class ModuleLens: ObservableObject {
     }
 
     private func applyFilterTo(image: UIImage, filterEffect: Filter) -> UIImage {
-        let context = CIContext()
-        let filter = CIFilter(name: filterEffect.filterName)
-        filter?.setValue(filterEffect.filterEffectValue!, forKey: filterEffect.filterEffectValueName!)
-        filter?.setValue(CIImage(image: image), forKey: kCIInputImageKey)
-        guard let output = filter?.outputImage else {
+//        let context = CIContext()
+        guard context != nil else {
+            print("###----> CONTECT NOT FOUND")
+            return image
+        }
+        
+        filterEffect.setValue(CIImage(image: image), forKey: kCIInputImageKey)
+        guard let output = filterEffect.outputImage else {
             return image
         }
         return UIImage(cgImage: context.createCGImage(output, from: output.extent) ?? (image as! CGImage))
@@ -63,9 +70,14 @@ class ModuleLens: ObservableObject {
     
     func processFilters(source: UIImage) -> UIImage {
         let filterArgs: ffRenderArguments = RandomRenderArguments.generate()
-        self.gaussianBlur.filterEffectValue = (filterArgs.leakTintRGB.g * 10000)/100 // this is a placeholder value
-        self.sepia.filterEffectValue = (filterArgs.cornerOpacity + 100)/100 // this is a placeholder value
-        let temp0 = self.processImage(image: source, filterEffect: self.gaussianBlur)
+        
+        self.gaussianBlur.setValue((filterArgs.leakTintRGB.g * 10000)/100, forKey: "inputRadius") // this is a placeholder value
+        self.sepia.setValue((filterArgs.cornerOpacity + 100)/100, forKey: "inputIntensity") // this is a placeholder value
+        self.colorClamp.setValuesForKeys(["inputMinComponents": CIVector(x: 0, y: 0, z: 0, w: 0),
+                                          "inputMaxComponents": CIVector(x: 1, y: 1, z: 1, w: 1)
+                                         ])
+        
+        let temp0 = self.processImage(image: source, filterEffect: self.colorClamp)
         let temp1 = self.processImage(image: temp0, filterEffect: self.gaussianBlur)
         let finalResult = self.processImage(image: temp1, filterEffect: self.sepia)
         print("###------------ RETURN FINAL RESULT!!!!!!")
